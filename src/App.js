d7806cd0a8c6ab3e2cbbfe3a6df89e95e5d422d3
mg-torres <54419199+mg-torres@users.noreply.github.com>
2024-07-31 17:30:48 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -4,20 +4,71 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Billi App</title>
+  <link rel="stylesheet" href="styles.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap"
+    rel="stylesheet"
+  />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
+    rel="stylesheet"
+  />
+  <div className="container">
+    <div className="content-wrapper">
+      <div className="header">
+        <img src="logo_website.png" alt="Billi Logo" className="logo" />
+      </div>
+      <div className="content">
+        <h1>Tú tomas la foto y nosotros hacemos el resto</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Registra tus gastos, fija tus metas de ahorro y descubre patrones que
+          te permitan mejorar tu vida financiera
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="signup">
+          <h2>Únete a nuestra lista de espera</h2>
+          <h3>
+            Sé una de las primeras personas en probar nuestra versión beta
+          </h3>
+          <div className="input-container">
+            <input
+              type="email"
+              placeholder="Regístrate regístrate regístrate regístrate"
+            />
+            <button>Sign up</button>
+          </div>
+        </div>
+        <div className="download-buttons">
+          <a href="#" className="button google-play">
+            <img src="google-play-logo.png" alt="Google Play" />
+            <span>
+              Disponible en
+              <br />
+              Google Play
+            </span>
+          </a>
+          <a href="#" className="button app-store">
+            <img src="apple-logo.png" alt="App Store" />
+            <span>
+              Disponible en
+              <br />
+              App Store
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div className="phone">
+      <img src="mockup.png" alt="Phone Image" className="phone-image" />
+    </div>
+  </div>
+</>
+
     </div>
   );
 }
