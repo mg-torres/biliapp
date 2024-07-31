b1716c29ab24ad749911c02dd64be0f3243811dc
mg-torres <54419199+mg-torres@users.noreply.github.com>
2024-07-31 18:06:10 -0500

    la tercera es la vencida 2.0

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   <div className="container">
     <div className="content-wrapper">
       <div className="header">
-        <img src="logo_website.png" alt="Billi Logo" className="logo" height="500px" />
+        <img src="logo_website.png" width={250} height={250} alt="Billi Logo" className="logo" />
       </div>
       <div className="content">
         <h1>Tú tomas la foto y nosotros hacemos el resto</h1>
