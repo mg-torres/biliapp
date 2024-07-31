aa977fbf657794cedf13afd44ee27efedc9744ed
mg-torres <54419199+mg-torres@users.noreply.github.com>
2024-07-31 18:09:24 -0500

    no scrollea

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   <div className="container">
     <div className="content-wrapper">
       <div className="header">
-        <img src="logo_website.png" width={50} height={50} alt="Billi Logo" className="logo" />
+        <img src="logo_website.png" width={10} height={10} alt="Billi Logo" className="logo" />
       </div>
       <div className="content">
         <h1>Tú tomas la foto y nosotros hacemos el resto</h1>
